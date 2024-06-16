1076433a1e2a917437920e07f8efe57ac65b00e3
epiland <pilandethan@gmail.com>
2024-06-16 17:20:36 -0400

    Feat!: Imported the make_sandwich and check_resources methods from the first sandwich maker assignment.

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -6,6 +6,16 @@ class SandwichMaker:
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
         #####
+        for ingredient, amount in ingredients.items():
+            if self.machine_resources.get(ingredient, 0) < amount:
+                print(f"Sorry, there is not enough {ingredient}.")
+                return False
+        return True
+
 
     def make_sandwich(self, sandwich_size, order_ingredients):
         ########
+        if sandwich_size in recipes:
+            for ingredient, amount in order_ingredients.items():
+                self.machine_resources[ingredient] -= amount
+
